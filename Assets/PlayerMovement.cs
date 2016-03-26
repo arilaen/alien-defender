b4f4b374a8bec95b098c81830fa16413ca877c50
Marcela Rodriguez <arilaen@gmail.com>
2016-03-26 16:05:47 -0400

    Camera follows player, player moves with mouse but not completely - needs updating

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	private Vector3 movement;
+	private Animator anim;
+	private Rigidbody playerRigidbody;
+	private int floorMask;
+	private float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Ground");
+		//anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		Turning ();
+		//Animating (h, v);
+	}
+	void Move(Vector3 movement)
+	{
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit floorHit;
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+			if (Input.GetMouseButton(0)) //Move
+			{
+				Move (floorHit.point);
+			}
+		}
+	}
+//	void Animating(float h, float v)
+//	{
+//		bool walking = (h != 0f || v != 0f);
+//		anim.SetBool ("IsWalking", walking);
+//	}
+}
