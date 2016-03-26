1f70b14c2c47cf4e8f48c7769e27b14e6daa168a
Marcela Rodriguez <arilaen@gmail.com>
2016-03-26 17:50:31 -0400

    Made spawning points for all objects so far

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnee;
+	public Transform[] spawnPoints;
+	public float spawnDelay = 2f;
+	public float spawnRate = 5f;
+	public float timeLimit = 16f;
+	
+	// Update is called once per frame
+	void Start () {
+		StartCoroutine ("SpawnRoutine");
+	}
+
+	IEnumerator SpawnRoutine() {
+		while (Time.realtimeSinceStartup < spawnDelay) {
+			yield return null;
+		}
+		while (Time.realtimeSinceStartup < timeLimit) {
+			Spawn ();
+			yield return new WaitForSeconds (spawnRate);
+		}
+		yield return null;
+	}
+
+	void Spawn ()
+	{
+		for (int i = 0; i < spawnPoints.Length; i++) {
+			Instantiate (spawnee, spawnPoints[i].position, spawnPoints[i].rotation);
+		}
+	}
+
+}
