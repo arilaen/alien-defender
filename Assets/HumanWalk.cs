5b0eb1875b64afc4928833c58cdcf75736b8cab5
paopow <ksiangliulue@gmail.com>
2016-03-26 17:01:09 -0400

    Add a simple human prefab

---
+++ Assets/HumanWalk.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+
+public class HumanWalk : MonoBehaviour {
+	public float moveTime = 0.1f;
+	private BoxCollider2D boxCollider;
+	private float inverseMoveTime; 
+
+	// Use this for initialization
+	void Start () {
+		boxCollider = GetComponent <BoxCollider2D> ();
+		inverseMoveTime = 1f / moveTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Whatever");
+	}
+
+//	bool move(int xDir, int yDir, int zDir){
+//		//TODO: let's do dumb walking for now
+//
+//	}
+}
