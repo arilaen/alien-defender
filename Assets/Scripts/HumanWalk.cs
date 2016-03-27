40953368d7a05d4120d6bfe050bd6fa3f50cb0c0
Marcela Rodriguez <arilaen@gmail.com>
2016-03-27 13:58:20 -0400

    Brought in sprites

---
+++ Assets/Scripts/HumanWalk.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+
+public class HumanWalk : MonoBehaviour {
+	public float moveTime = 0.1f;
+	private Rigidbody rb;
+
+	private float inverseMoveTime; 
+	// Use this for initialization
+	void Start () {
+		inverseMoveTime = 1f / moveTime;
+		rb = GetComponent<Rigidbody> ();
+//		rb.velocity = new Vector3 (0f, 0.1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+//		Debug.Log (rb.position);
+	}
+
+//	bool move(int xDir, int yDir, int zDir){
+//		//TODO: let's do dumb walking for now
+//
+//	}
+}
+	
